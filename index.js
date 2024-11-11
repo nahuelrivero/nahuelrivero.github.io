74b1e587294d8b905f152f87ac1caa17aed105a7
nahuelrivero <58092518+nahuelrivero@users.noreply.github.com>
2024-11-10 23:40:34 -0300

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const d = document;
-
-const nav = d.getElementById('nav');
-const hamburguer = d.getElementById("hamburguer");
-
-hamburguer.addEventListener("click", (e) => {
-    hamburguer.classList.toggle('is-active')
-    nav.classList.toggle('active-menu')
-})
